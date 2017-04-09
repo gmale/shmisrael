d8c22a5a306918629f9fa3c58e5750df883e723d
Kevin Gorham <anothergmale@gmail.com>
2017-04-09 02:06:40 -0600

    failed to work

--- src/components/ItineraryMap/index.js
+++ src/components/ItineraryMap/index.js
@@ -1,9 +1,25 @@
 import React from "react"
 
 // import styles from "./index.css"
+import GoogleMapReact from 'google-map-react';
+
+// const AnyComp = () => (<div>hi</div>)
 
 const ItineraryMap = () => (
-  <div>Hello Comps!</div>
+  // <GoogleMap bootstrapURLKeys={{key: "AIzaSyAQ38AyTgvhBYPw4rf1ZPcGZxrIpZoQ0sI"}}
+  //   center={[31.778031, 35.235355]} zoom={8} />
+
+<div width="400" height="400">
+    <GoogleMapReact
+          bootstrapURLKeys={{key: "AIzaSyAQ38AyTgvhBYPw4rf1ZPcGZxrIpZoQ0sI", language:'en'}}
+          center={[31.778031, 35.235355]}
+          zoom={8}
+        />
+</div>
 )
+
+// ItineraryMap.propTypes = {
+//   text: PropTypes.string,
+// }
 
 export default ItineraryMap
